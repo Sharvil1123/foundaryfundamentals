a2fae54e84e372ae68f6884d983d46abc4f9f37f
Sharvil1123 <sharvilbhalke@gmail.com>
2024-03-16 20:58:41 +0530

    comments

---
+++ foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns(SimpleStorage){
+        vm.startBroadcast(); // start of transcaction
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast(); // end of transcaction
+        return simpleStorage;
+    } // run command forge script "script location" to deploy the script
+}
+// commands.
+
+// forge script [script location] --rpc-url [rpc url] --broadcast --private-key [pvt key] - command to deploy on local blockchain.
